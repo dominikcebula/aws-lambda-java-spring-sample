4c173408d6aa6203a34a8200a8f22065199f7f78
Dominik Cebula <dominikcebula@gmail.com>
2024-09-10 14:39:45 +0200

    aws-lambda-java-spring-sample

--- products-catalog-e2e-tests/src/test/java/com/dominikcebula/samples/aws/lambda/products/catalog/ProductsCatalogTest.java
+++ products-catalog-e2e-tests/src/test/java/com/dominikcebula/samples/aws/lambda/products/catalog/ProductsCatalogTest.java
@@ -1,4 +1,42 @@
 package com.dominikcebula.samples.aws.lambda.products.catalog;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.services.cloudformation.CloudFormationClient;
+import software.amazon.awssdk.services.cloudformation.model.DescribeStacksRequest;
+import software.amazon.awssdk.services.cloudformation.model.DescribeStacksResponse;
+import software.amazon.awssdk.services.cloudformation.model.Output;
+import software.amazon.awssdk.services.cloudformation.model.Stack;
+
+import java.util.List;
+
 public class ProductsCatalogTest {
+    private static String apiEndpoint;
+
+    @BeforeAll
+    static void setUp() {
+        apiEndpoint = getApiEndpoint();
+    }
+
+    @Test
+    void shouldCreateAndRetrieveProduct() {
+        // TODO
+    }
+
+    private static String getApiEndpoint() {
+        try (CloudFormationClient cloudFormationClient = CloudFormationClient.create()) {
+            DescribeStacksResponse response = cloudFormationClient.describeStacks(DescribeStacksRequest.builder()
+                    .stackName("products-catalog-lambda-app")
+                    .build());
+
+            return response.stacks().stream()
+                    .filter(stack -> stack.stackName().equals("products-catalog-lambda-app"))
+                    .map(Stack::outputs)
+                    .flatMap(List::stream)
+                    .map(Output::outputKey)
+                    .filter(s -> s.equals("ApiEndpoint"))
+                    .findFirst()
+                    .orElseThrow(() -> new IllegalStateException("Unable to find API Endpoint for testing based on Cloud Formation Stack"));
+        }
+    }
 }
